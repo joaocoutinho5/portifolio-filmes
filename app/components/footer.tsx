5a15160a071b29042f742fc1a5d12e2dd9fe7a12
João Pedro <joaopedro.b.c@hotmail.com>
2026-01-23 01:25:36 -0300

    Adicionando setas com animações no footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import { Heart } from "lucide-react";
+import { Heart, ArrowUpRight } from "lucide-react";
 
 export default function Footer() {
   return (
     //Footer Wrapper
     <footer className="w-full bg-[#141414] border-t border-[#2E2E2E] text-[#999999] py-4">
-      <div className="w-full flex flex-col md:flex-row items-center md:items-center justify-between gap-4 md:gap-0  max-w-[1560px] mx-auto px-5">
+      <div className="w-full flex flex-col md:flex-row items-center md:items-center justify-between gap-4 md:gap-0 max-w-[1560px] mx-auto px-5">
         <p className="text-md text-center md:text-left flex flex-row items-center gap-1">
           Criado com{" "}
           <Heart size={20} fill="currentColor" className="text-[#e50914]" /> por{" "}
@@ -14,22 +14,38 @@ export default function Footer() {
 
         {/* Links */}
         <nav className="flex items-center gap-6 text-sm">
-          <Link href="" className="hover:text-white transition" target="_blank">
+          <Link
+            href=""
+            className="group flex flex-row items-center justify-center gap-0.5 hover:text-white transition"
+            target="_blank"
+          >
             Portfólio
+            <ArrowUpRight
+              size={18}
+              className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition"
+            />
           </Link>
           <Link
             href="https://www.linkedin.com/in/joaocoutinho5"
-            className="hover:text-white transition"
+            className="group flex flex-row items-center justify-center gap-0.5 hover:text-white transition"
             target="_blank"
           >
             LinkedIn
+            <ArrowUpRight
+              size={18}
+              className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition"
+            />
           </Link>
           <Link
             href="https://github.com/joaocoutinho5"
-            className="hover:text-white transition"
+            className="group flex flex-row items-center justify-center gap-0.5 hover:text-white transition"
             target="_blank"
           >
             GitHub
+            <ArrowUpRight
+              size={18}
+              className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition"
+            />
           </Link>
         </nav>
       </div>
